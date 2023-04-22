6ab8ff79a4ec9f19849b2544191849a0b6d41ea3
davidcharles7 <127146486+davidcharles7@users.noreply.github.com>
2023-04-22 02:53:54 -0700

    Fully working

--- settings_test.py
+++ settings_test.py
@@ -1,10 +1,10 @@
 WIDTH = 800
 HEIGHT = 600
-PLAYER_ACC = 2
+PLAYER_ACC = 0.8
 PLAYER_FRICTION = -0.12
 PLAYER_JUMP = 15
 PLAYER_GRAV = 0.8
-MOB_ACC = 2
+MOB_ACC = 1
 MOB_FRICTION = -0.12
 BLACK = (0,0,0)
 WHITE = (255,255,255)
@@ -21,9 +21,9 @@ PAUSED = False
 # Starting platforms
 PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 450, ORANGE, "danger"),
                  (0, HEIGHT * 3 / 4, 100, 20, GRAY, "normal"),
-                 (150, 540 , 100, 20, GREEN, "bouncey"),
+                 (150, 330 , 100, 20, GREEN, "bouncey"),
                  (700, HEIGHT * 3 / 4, 100, 20, GRAY, "normal"),
-                 (125, HEIGHT - 350, 100, 5, GREEN, "disappearing "),
-                 (350, 200, 100, 20, GREEN, "normal"),
-                 (175, 100, 50, 20, GREEN, "normal"),
+                 (500, HEIGHT * 3 / 4, 100, 20, GRAY, "normal"),
+                 (350, 200, 100, 20, GRAY, "normal"),
+                 (175, 100, 50, 20, GRAY, "normal"),
                 ]
